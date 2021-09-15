b6a2f2d7c371e10c9c4d2d75d55588ca045554ba
Dragana Radulovic <dradulovic@Draganas-MBP.vc.shawcable.net>
2021-09-15 15:36:20 -0700

    data update and pop up info cleanup

--- src/stressmap.js
+++ src/stressmap.js
@@ -247,22 +247,74 @@ function onEachFeature(feature, layer) {
     // }
 
     // for debug
-    // if (feature.properties.id == 'way/103095749'){
+    // if (feature.properties.id == 'way/35198494'){
     //   console.log('Dragana:: tag ' + JSON.stringify(feature.properties))
     // }
 
+    // customize value for category (road category) tag
+    // options based on analysis of Sept 2021 designated data (StressDataExploration.R)
+    let highwayValueToShow = null
+    let categoryValueToShow = null
     if (feature.properties.highway) {
-      // customize title for road categories
       let highwayValue = feature.properties.highway
-      let highwayValueToShow = feature.properties.highway
-      if (highwayValue == "motorway" || highwayValue == "trunk" || highwayValue == "primary" || 
-          highwayValue == "secondary" || highwayValue == "tertiary" || 
+
+      if (highwayValue == "path" || highwayValue == "cycleway" || highwayValue == "footway"){
+        // separated bike infrastructure
+        let footValue = feature.properties.foot
+        if (footValue == "designated" ||  footValue == "yes" || footValue == "permissive" || footValue == "yes;permissive"){
+          footValue = "yes"
+        }else{
+          footValue = "no"
+        }
+        let bicycleValue = feature.properties.bicycle
+        if (bicycleValue == "designated" ||  bicycleValue == "yes" || bicycleValue == "permissive" || bicycleValue == "yes;permissive"){
+          bicycleValue = "yes"
+        }else{
+          bicycleValue = "no"
+        }
+        // path
+        if (highwayValue == "path"){
+          if (footValue == "yes" && bicycleValue == "yes"){
+            categoryValueToShow = "shared path"
+          }else{
+            categoryValueToShow = "path"
+          }
+        // cycleway
+        }else if (highwayValue == "cycleway"){
+          if (footValue == "yes"){
+            categoryValueToShow = "shared"
+          }else{
+            categoryValueToShow = "bike"
+          }
+          // cycleway crossings
+          let cyclewayValue = feature.properties.cycleway
+          if (cyclewayValue == "crossing"){
+            categoryValueToShow += " crossing"
+          }else{
+            categoryValueToShow += " path"
+          }
+          // footway
+        }else if (highwayValue == "footway"){
+          if (bicycleValue == "yes"){
+            categoryValueToShow = "shared"
+          }else{
+            categoryValueToShow = "foot"
+          }
+          // footway crossings
+          let footwayValue = feature.properties.footway
+          if (footwayValue == "crossing"){
+            categoryValueToShow += " crossing"
+          }else{
+            categoryValueToShow += " path"
+          }
+        }
+      }else if (highwayValue == "motorway" || highwayValue == "trunk" || highwayValue == "primary" || 
+          highwayValue == "secondary" || highwayValue == "tertiary" || highwayValue == "service" ||
           highwayValue == "unclassified" || highwayValue == "residential" || 
           highwayValue == "motorway_link" || highwayValue == "trunk_link" || highwayValue == "primary_link" ||
           highwayValue == "secondary_link" || highwayValue == "tertiary_link"){
-
-            popupContent += "<b>road category: </b>";
-            if (highwayValue != "unclassified" && highwayValue != "residential"){
+            // roads  
+            if (highwayValue != "unclassified" && highwayValue != "residential" && highwayValue != "service"){
               highwayValueToShow = "highway"
             }
             if (highwayValue == "tertiary" || highwayValue == "tertiary_link"){
@@ -271,18 +323,125 @@ function onEachFeature(feature, layer) {
             if (highwayValue == "secondary" || highwayValue == "secondary_link"){
               highwayValueToShow = "arterial"
             }
+            if (highwayValueToShow == null){
+              highwayValueToShow = highwayValue
+            }
+            // if "service" then add what kind of service
+            if (highwayValue == "service"){
+              if (feature.properties.service) {
+                let serviceValue = feature.properties.service
+                if (serviceValue == "parking_aisle"){
+                  serviceValue = "parking aisle"
+                }
+                highwayValueToShow += "<br><b>service: </b>";
+                highwayValueToShow += serviceValue;
+              }
+            }
+            // figure out bike infra categories ("cycleway" tag)
+            let cyclewayValue = null
+            if (feature.properties.cycleway) {
+              cyclewayValue = feature.properties.cycleway
+              // check if there's cycleway.both, cycleway.right, cycleway.left
+            }else if(feature.properties["cycleway.both"]){
+              cyclewayValue = feature.properties["cycleway.both"]
+            }else if(feature.properties["cycleway.right"]){
+              cyclewayValue = feature.properties["cycleway.right"]
+            }else if(feature.properties["cycleway.left"]){
+              cyclewayValue = feature.properties["cycleway.left"]
+            }
+            if (cyclewayValue){
+              if (cyclewayValue == "shared_lane"  || cyclewayValue == "shared" || cyclewayValue == "share_busway"){
+                categoryValueToShow = "shared lane"
+              }
+              if (cyclewayValue == "lane"){
+                categoryValueToShow = "painted lane"
+              }              
+              if (cyclewayValue == "track"){
+                categoryValueToShow = "protected lane"
+              }
+              if (cyclewayValue == "crossing"){
+                categoryValueToShow = "crossing"
+              }
+            }
       }else{
-        popupContent += "<b>category: </b>";
+        // everything else
+        categoryValueToShow = highwayValue
       }
+    }
+    // add bike category first
+    if (categoryValueToShow){
+      popupContent += "<b>category: </b>";
+      popupContent += categoryValueToShow;
+    }
+    // add name second
+    if (feature.properties.name){
+      // add new line if there's something in front of it
+      if (popupContent != ""){
+        popupContent += "<br>"
+      }
+      popupContent += "<b>name: </b>";
+      popupContent += feature.properties.name;
+    }
+    // then road category
+    if (highwayValueToShow){
+      if (popupContent != ""){
+        popupContent += "<br>"
+      }
+      popupContent += "<b>road category: </b>";
       popupContent += highwayValueToShow;
     }
-    for (let property in feature.properties) {
-        //console.log('Dragana:: tag ' + JSON.stringify(tag) +', value: '+ way.tags[tag])
-      if ((property !== "id") && (property !== "decisionMsg") && (property !== "highway") && (feature.properties[property] != null)){
-        popupContent += "<br><b>" + property + ": </b>";
-        popupContent += feature.properties[property];
+    // add surface and maxspeed
+    if (feature.properties.surface){
+      if (popupContent != ""){
+        popupContent += "<br>"
       }
+      popupContent += "<b>surface: </b>";
+      let surfaceValue = feature.properties.surface
+      if (surfaceValue == "paving_stones"){
+        surfaceValue = "paving stones"
+      }else if (surfaceValue == "fine_gravel"){
+        surfaceValue = "fine gravel"
+      }else if (surfaceValue == "unhewn_cobblestone"){
+        surfaceValue = "unhewn cobblestone"
+      }else if (surfaceValue == "grass_paver"){
+        surfaceValue = "grass paver"
+      }
+      popupContent += surfaceValue;
     }
+    if (feature.properties.maxspeed){
+      if (popupContent != ""){
+        popupContent += "<br>"
+      }
+      popupContent += "<b>max speed: </b>";
+      popupContent += feature.properties.maxspeed;
+    }
+    // add lit and incline
+    if (feature.properties.lit){
+      if (popupContent != ""){
+        popupContent += "<br>"
+      }
+      popupContent += "<b>lit: </b>";
+      popupContent += feature.properties.lit;
+    }
+    if (feature.properties.incline){
+      if (popupContent != ""){
+        popupContent += "<br>"
+      }
+      popupContent += "<b>incline: </b>";
+      popupContent += feature.properties.incline;
+    }
+
+    // FOR TEST
+  // popupContent += "<br>=================";
+  //   for (let property in feature.properties) {
+  //       //console.log('Dragana:: tag ' + JSON.stringify(tag) +', value: '+ way.tags[tag])
+  //     //if ((property != "id") && (property != "decisionMsg") && (property != "access")
+  //     //  (property != "highway") && (feature.properties[property] != null)){
+        
+  //         popupContent += "<br><b>" + property + ": </b>";
+  //       popupContent += feature.properties[property];
+  //     //}
+  //  }
     // for this mobile version don't show decision message
     // if (feature.properties.decisionMsg) {
     //   popupContent += "<br><br><b>Decision Msg: </b>";
